455fa4d425f9f19537169825d2a60ae0d671f0ab
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-05-29 14:30:28 +0300

    Rewrote function to assembly

--- Assembly/assy2.cpp
+++ Assembly/assy2.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
-long long f2(int x)
-{
-    long long sum = 0;
-    for (int i = 1; i <= x; ++i)
-    {
-        sum += i * (i + 1);
-    }
-    return sum;
-}
+extern "C" long long f2(int x);
 
 int main()
 {
@@ -20,4 +12,33 @@ int main()
     std::cout << "The value of the function f2 at x = " << x << " is: f2(" << x << ") = " << result << std::endl;
 
     return 0;
+}
+
+extern "C" long long f2(int x)
+{
+    long long sum = 0;
+    int i;
+    __asm__(
+        "movl $1, %%ecx\n"
+        "movq $0, %%rax\n"
+
+        "start_loop:\n"
+        "cmp %%ecx, %1\n"
+        "jg end_loop\n"
+
+        "imul %%ecx, %%ecx\n"
+        "add %%ecx, %%ecx\n"
+        "add %%ecx, %%rax\n"
+
+        "inc %%ecx\n"
+        "jmp start_loop\n"
+
+        "end_loop:\n"
+        "movq %%rax, %0\n"
+
+        : "=r"(sum), "=r"(x)
+        : "1"(x)
+        : "%rax", "%rcx", "cc");
+
+    return sum;
 }
